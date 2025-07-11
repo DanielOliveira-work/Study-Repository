e2da6c27d9403a49f86cab947791769fd5b86780
Daniel Silva de Oliveira <work.doliveira@gmail.com>
2025-07-10 21:49:20 -0300

    Exercicio finalizado!
    Correção e finalização do exercicio de boletim de estudante.

--- Exercicio_10.py
+++ Exercicio_10.py
@@ -1,16 +1,17 @@
 def report_card(info):
-    #total, maior, menor, média e se foi aprovado com boleano.
-    bigger = minor = total = 0
-    for itens, value in info.items():
-        total += value
-        if itens == 0 or value > bigger:
-           bigger = value
-        if itens == 0:
-            minor == value
-    average = total / len(info) 
-    print(f'The {name} > Test quantities: {len(info)}, highest grade: {bigger}, lowest grade: {minor}, average: {average}')
-
-##CONTINUAR AMANHA, TRUE OR FALSE 
+    """
+    :info - student information. 
+    """
+    bigger = max(info.values()) 
+    minor = min(info.values())
+    average = (bigger + minor) / len(info)
+    if average > 7:
+        end_course = 'APROVADO'
+    elif 5 < average < 7:
+        end_course = 'RECUPERAÇÃO'
+    else:
+        end_course = 'REPROVADO'
+    print(f'The {name} > Test quantities: {len(info)}, highest grade: {bigger}, lowest grade: {minor}, average: {average:.1f} \n{end_course}')
 
 info = {}
 
