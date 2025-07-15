acbdedef0e6e0ed335587da1b1f8e3986e48196f
Daniel Silva de Oliveira <work.doliveira@gmail.com>
2025-07-15 19:26:18 -0300

    Atualização do módulo
    Aprendendo mais sobre módulos

--- Módulo/def_conver.py
+++ Módulo/def_conver.py
@@ -12,6 +12,11 @@ def increase(coin):
     increase = coin + (coin * 0.10)
     return increase
 
+
 def discount(coin):    
     value_discount = coin - (coin * 0.13)   
     return value_discount
+
+
+def coin(price=0, coin='R$'):
+    return f'{coin}{price:.2f}'.replace('.',',')
