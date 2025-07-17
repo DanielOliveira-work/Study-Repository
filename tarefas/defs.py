64965b08c5bc22a702b471c651c2e181c9d7b93c
Daniel Silva de Oliveira <work.doliveira@gmail.com>
2025-07-17 13:39:30 -0300

    To Do task
    Primeiro mini-projeto

--- tarefas/defs.py
+++ tarefas/defs.py
@@ -1,1 +1,8 @@
+def lines():
+    print('-' * 30)
 
+def menu():
+    print('Enter 1 to add a task')
+    print('Enter 2 to change the status of a task')
+    print('Enter 3 to delete a task')
+    print('Enter 4 to exit the program')
--- tarefas/tarefas.py
+++ tarefas/tarefas.py
@@ -1,5 +1,36 @@
+import defs
+
+lista = []
+task = {}
 ## Criar um programa que permita cadastrar tarefas do dia com status de conclusão.
 
+defs.lines()
+print('To Do List')
+defs.lines()
 
+defs.menu()
 
-
+while True:
+    option = int(input('Select the desired option: '))
+    match option:
+        case 1: #adicionar tarefa
+            name = input('Enter the name of the desired task: ')
+            status = int(input('Select task status: \n1 for pendant. \n2 for completed \n-  '))
+            if status == 1:
+                status = 'pendant'
+            elif status == 2:
+                status = 'completed'
+            task = {
+                "task_name": name,
+                "status": status
+            }
+            lista.append(task)
+            last = lista[-1]
+            print(f'Task {last["task_name"]} with status {last["status"]} added successfully')
+        case 2: #alterar status
+            #print('Which task do you want to change?')
+            if lista[task] == None:
+                print('Void list')
+            else:
+                for task in lista[task]:
+                    print(f'{task}')
